755d73a8cd3521d25f61baa4a48bdc2655943248
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2023-09-22 00:37:19 +0530

    few changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section id="contact" class="bg-white dark:bg-gray-900">
+    <section id="contact" class="bg-white dark:bg-gray-900" >
       <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">
           Contact Us
         </h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
